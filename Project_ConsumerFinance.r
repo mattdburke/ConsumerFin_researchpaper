c78d3ee4cb0f1957e6438c1c61d5b85312dde094
Matt Burke <39552849+mattdburke@users.noreply.github.com>
2021-07-06 17:29:42 +0100

    Update Project_ConsumerFinance.r

--- Project_ConsumerFinance.r
+++ Project_ConsumerFinance.r
@@ -15,6 +15,14 @@ setwd("C:/Users/mattb/Documents/GitHub/ConsumerFin_researchpaper")
 library(psych)
 library(multcomp)
 
+lmp <- function (modelobject) {
+    if (class(modelobject) != "lm") stop("Not an object of class 'lm' ")
+    f <- summary(modelobject)$fstatistic
+    p <- pf(f[1],f[2],f[3],lower.tail=F)
+    attributes(p) <- NULL
+    return(p)
+}
+
 df1<-read.csv("data.csv", header=TRUE)
 
 df1[3:12] <- lapply(df1[3:12], as.numeric)
@@ -52,99 +60,105 @@ options(scipen=2)
 table_2 <- as.data.frame(table_2)
 table_2
 
+### Table 3 Hypothesis test
 
-ID<-data.frame[,1]
-APRexplain<-data.frame[,2]
-ComplexCount<-data.frame[,3]
-FinancetoComplex<-data.frame[,4]
-F<-data.frame[,5]
-FK<-data.frame[,6]
-Fog<-data.frame[,7]
-Sentcount<-data.frame[,8]
-SuperComplexCount<-data.frame[,9]
-SuperFinancetoComplex<-data.frame[,10]
-Terms<-data.frame[,11]
-Wordcount<-data.frame[,12]
-Category<-data.frame[,13]
-data.frame["TermstoWords"]<-Terms/Wordcount
-data.frame["PCcomplex"]<-ComplexCount/Wordcount
-data.frame["SPCcomplex"]<-SuperComplexCount/Wordcount
-data.frame["WordsperSentence"]<-Wordcount/Sentcount
-TermstoWords<-Terms/Wordcount
-PCcomplex<-ComplexCount/Wordcount
-SPCcomplex<-SuperComplexCount/Wordcount
-WordsperSentence<-Wordcount/Sentcount
+payday.t <- t.test(payday$FogIndex, mu = 12)[1]
+payday.p <- t.test(payday$FogIndex, mu = 12)[3]
+personal.t <- t.test(personal$FogIndex, mu = 12)[1]
+personal.p <- t.test(personal$FogIndex, mu = 12)[3]
+credit.t <- t.test(credit$FogIndex, mu = 12)[1]
+credit.p <- t.test(credit$FogIndex, mu = 12)[3]
 
-describeBy(data.frame, group=data.frame$Category, mat=TRUE, digits=4)
+table_3 <- rbind( c(payday.t , payday.p),
+					c(personal.t, personal.p),
+					c(credit.t, credit.p))
+rownames(table_3)<-c("payday", "personal", "credit")
+table_3
 
-Category = factor(Category)
+### Table 4 Panel 1
 
-lm.fit.Fog <- lm(Fog~Category)
-lm.fit.TermstoWords <- lm(TermstoWords~Category)
-lm.fit.PCcomplex <- lm(PCcomplex~Category)
-lm.fit.FinancetoComplex <- lm(FinancetoComplex~Category)
-lm.fit.WordsperSentence <- lm(WordsperSentence~Category)
-lm.fit.Wordcount <- lm(Wordcount~Category)
+df1$Category = factor(df1$Category)
 
-# Results of the baseline linear model
-summary(lm.fit.Fog)
-summary(lm.fit.TermstoWords)
-summary(lm.fit.PCcomplex)
-summary(lm.fit.FinancetoComplex)
-summary(lm.fit.WordsperSentence)
-summary(lm.fit.Wordcount)
+lm.fit.Fog <- lm(FogIndex~Category, data=df1)
+lm.fit.TermstoWords <- lm(TermstoWords~Category, data=df1)
+lm.fit.PCcomplex <- lm(PCcomplex~Category, data=df1)
+lm.fit.FinancetoComplex <- lm(FinancetoComplex~Category, data=df1)
+lm.fit.WordsperSentence <- lm(WordsperSentence~Category, data=df1)
+lm.fit.WordCount <- lm(WordCount~Category, data=df1)
+
+FogIndex.F <- summary(lm.fit.Fog)$fstatistic[1]
+FogIndex.p <- lmp(lm.fit.Fog)
+TermstoWords.F <- summary(lm.fit.TermstoWords)$fstatistic[1]
+TermstoWords.p <- lmp(lm.fit.TermstoWords)
+PCcomplex.F <- summary(lm.fit.PCcomplex)$fstatistic[1]
+PCcomplex.p <- lmp(lm.fit.PCcomplex)
+FinancetoComplex.F <- summary(lm.fit.FinancetoComplex)$fstatistic[1]
+FinancetoComplex.p <- lmp(lm.fit.FinancetoComplex)
+WordsperSentence.F <- summary(lm.fit.WordsperSentence)$fstatistic[1]
+WordsperSentence.p <- lmp(lm.fit.WordsperSentence)
+WordCount.F <- summary(lm.fit.WordCount)$fstatistic[1]
+WordCount.p <- lmp(lm.fit.WordCount)
+
+Table_4_1 <- rbind( c(FogIndex.F, FogIndex.p),    
+					c(TermstoWords.F, TermstoWords.p),
+					c(PCcomplex.F, PCcomplex.p),
+					c(FinancetoComplex.F, FinancetoComplex.p),
+					c(WordsperSentence.F, WordsperSentence.p),
+					c(WordCount.F, WordCount.p))
+Table_4_1
+
+### Table 4 Panel 2
 
 summary(glht(lm.fit.Fog, mcp(Category="Tukey")))
 summary(glht(lm.fit.TermstoWords, mcp(Category="Tukey")))
 summary(glht(lm.fit.PCcomplex, mcp(Category="Tukey")))
 summary(glht(lm.fit.FinancetoComplex, mcp(Category="Tukey")))
 summary(glht(lm.fit.WordsperSentence, mcp(Category="Tukey")))
-summary(glht(lm.fit.Wordcount, mcp(Category="Tukey")))
-
-# Robustness
-# Only doing robustness on the variables that 
-# demonstrated significance in the baseline
-
-glm.fit.Fog <- glm(Fog~Category, family=Gamma(link='log'))
-glm.fit.TermstoWords <- glm(TermstoWords~Category, family=Gamma(link='log'))
-glm.fit.PCcomplex <- glm(PCcomplex~Category, family=Gamma(link='log'))
-glm.fit.FinancetoComplex <- glm(FinancetoComplex~Category, family=Gamma(link='log'))
-glm.fit.WordsperSentence <- glm(WordsperSentence~Category, family=Gamma(link='log'))
-glm.fit.Wordcount <- glm(Wordcount~Category, family=Gamma(link='log'))
-
-summary(glm.fit.Fog)
-summary(glm.fit.TermstoWords)
-summary(glm.fit.PCcomplex)
-summary(glm.fit.FinancetoComplex)
-summary(glm.fit.WordsperSentence)
-summary(glm.fit.Wordcount)
-
-summary(glht(glm.fit.Fog, mcp(Category="Tukey")))
-summary(glht(glm.fit.TermstoWords, mcp(Category="Tukey")))
-summary(glht(glm.fit.PCcomplex, mcp(Category="Tukey")))
-summary(glht(glm.fit.FinancetoComplex, mcp(Category="Tukey")))
-summary(glht(glm.fit.WordsperSentence, mcp(Category="Tukey")))
-summary(glht(glm.fit.Wordcount, mcp(Category="Tukey")))
-
-# Tests for the null hypothesis that readability = 12
-
-payday_fog<-(data.frame$Fog[which(data.frame$Category == 0)])
-personal_fog<-(data.frame$Fog[which(data.frame$Category == 1)])
-credit_fog<-(data.frame$Fog[which(data.frame$Category == 2)])
-t.test(payday_fog, mu=12)
-t.test(personal_fog, mu=12)
-t.test(credit_fog, mu=12)
+summary(glht(lm.fit.WordCount, mcp(Category="Tukey")))
 
 # Bartlett test for homogeneity of variance
 
-bartlett.test(Fog~Category)
-bartlett.test(TermstoWords~Category)
-bartlett.test(PCcomplex~Category)
-bartlett.test(FinancetoComplex~Category)
-bartlett.test(WordsperSentence~Category)
-bartlett.test(Wordcount~Category)
+bartlett.test(FogIndex~Category, data=df1)
+bartlett.test(TermstoWords~Category, data=df1)
+bartlett.test(PCcomplex~Category, data=df1)
+bartlett.test(FinancetoComplex~Category, data=df1)
+bartlett.test(WordsperSentence~Category, data=df1)
+bartlett.test(WordCount~Category, data=df1)
 
 # Principal component analysis and plot
+
+
+
+
+xpca <- data.frame(df1$FogIndex, 
+		df1$TermstoWords, 
+		df1$PCcomplex, 
+		df1$FinancetoComplex, 
+		df1$WordsperSentence, 
+		df1$WordCount)
+
+princompbjf<-princomp(xpca, cor=TRUE, scores=TRUE)
+
+princompbjf <- princompbjf$rotation
+
+summary(princompbjf)
+
+loadings(princompbjf)
+
+pc1<-princompbjf$scores[,1]
+
+pc2<-princompbjf$scores[,2]
+
+pca.frame <- data.frame(pc1, pc2)
+
+pch <- rep(c("0", "1", "2"), c(31, 31, 31))
+
+with(pca.frame, plot(pc1, pc2, pch=pch, xlab="Score on first principle component", ylab="Score on second principle component"))
+
+legend(0, -4, c("Pay Day Loan", "Personal Loan", "Credit Card"), pch=c("0","1","2"))
+
+
+
 
 pc1<-princompbjf$scores[,1]
 pc2<-princompbjf$scores[,2]
@@ -152,6 +166,7 @@ pca.frame <- data.frame(pc1, pc2)
 pch <- rep(c("0", "1", "2"), c(31, 31, 31))
 with(pca.frame, plot(pc1, pc2, pch=pch, xlab="Score on first principle component", ylab="Score on second principle component"))
 legend(0, -4, c("Pay Day Loan", "Personal Loan", "Credit Card"), pch=c("0","1","2"))
+
 
 # Analysis of credit card corpus
 
